d008f5105ffde78c87a6053456a139645c8cc8da
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2022-10-30 11:24:03 +0600

    forwarding letter

---
+++ resources/views/forwarding_letter/create.blade.php
@@ -0,0 +1,92 @@
+@extends('layout.main')
+@section('content')
+<section class="forms">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h4>Forwarding Letter</h4>
+                    </div>
+                    <div class="card-body">
+                        <p class="italic"><small>{{trans('file.The field labels marked with * are required input fields')}}.</small></p>
+                        {!! Form::open(['route' => ['forwarding-letter.store'], 'method' => 'post']) !!}
+                            <div class="row">
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.account')}} *</label>
+                                    <input type="date" name="date" class="form-control">
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.account')}} *</label>
+                                    <select name="account_id" id="" class="form-control">
+                                        <option value="">Select Account</option>
+                                        @foreach ($accounts as $account)
+                                            <option value="{{ $account->id }}">{{ $account->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.Lc No')}} *</label>
+                                    <select name="export_id" id="export-id" class="form-control">
+                                            <option value="">Select Lc No</option>
+                                        @foreach ($exports as $export)
+                                            <option value="{{ $export->id }}">{{ $export->lc_number }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.Value')}} *</label>
+                                    <input type="number" name="value" id="value" class="form-control" readonly>
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.Invoice No')}} *</label>
+                                    <input type="text" name="invoice_no" id="invoice-no" class="form-control" readonly>
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label>{{trans('file.Invoice Date')}} *</label>
+                                    <input type="date" name="invoice_date" id="invoice-date" class="form-control" readonly>
+                                </div>
+                            </div>
+                        <div class="form-group">
+                            <label>{{trans('file.Purpose')}}</label>
+                            <textarea name="purpose" rows="3" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">{{trans('file.submit')}}</button>
+                        </div>
+                        {{ Form::close() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script>
+    $(document).ready(function(){
+        $('#export-id').on('change', function(){
+            var exportId= $('#export-id').val();
+            var url = '{{ route("get-export") }}';
+
+            if(exportId){
+                $.ajax({
+                    type: "GET",
+                    dataType: "json",
+                    url: url,
+                    data: {
+                        exportId: exportId
+                    },
+                    success: function(data){
+                        console.log(data);
+                        $('#value').val(data.invoice_value);
+                        $('#invoice-no').val(data.invoice_no);
+                        $('#invoice-date').val(data.date);
+                    }
+
+                });
+            }
+        });
+    });
+</script>
+
+@endsection
